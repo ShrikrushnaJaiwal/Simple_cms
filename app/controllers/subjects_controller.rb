7b828c08e42278d47a67ffb88410da8b8f7b3a88
Shrikrushna <shrikrushna2107@gmail.com>
2012-09-26 03:35:20 +0530

    InItial Commit

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,9 @@
+class SubjectsController < ApplicationController
+	def list
+	    @subjects=Subject.order("subjects.position ASC")
+	end
+	
+	def show
+            @subject=Subject.find(params[:id])
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = '524ff11c4d8a0d8c4e276cc66293597b44f15c5180cc2ee200ece91a0c2d047ed538b4b8597ebeb028e63e9aca89cd7c91a4d302f3851a429e71bc90a0380663'
