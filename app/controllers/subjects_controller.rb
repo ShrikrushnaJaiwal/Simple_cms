0402d6128f7d6af48c0deaa5f105e7b51b8ee5af
ShrikrushnaJaiwal <shrikrushna2107@gmail.com>
2012-09-26 04:48:20 +0530

    Listing Subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,9 +1,16 @@
 class SubjectsController < ApplicationController
+        def index
+          list 
+          render 'list'
+        end
 	def list
 	    @subjects=Subject.order("subjects.position ASC")
 	end
 	
 	def show
             @subject=Subject.find(params[:id])
+	end
+	
+	def new
 	end
 end
